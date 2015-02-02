0e80ef1ed5a9e0add7d4f2950c2358c453763f8f
idugalic <idugalic@westum.com>
2015-02-02 11:48:12 +0100

    Run script and Index page ghanged

--- myalfresco/run.sh
+++ myalfresco/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Downloads the spring-loaded lib if not existing and runs the full all-in-one
 # (Alfresco + Share + Solr) using the runner project
-springloadedfile=~/.m2/repository/org/springframework/springloaded/1.2.0.RELEASE/springloaded-1.2.0.RELEASE.jar
-
+#springloadedfile=~/.m2/repository/org/springframework/springloaded/1.2.0.RELEASE/springloaded-1.2.0.RELEASE.jar
+springloadedfile=C:/Users/idugalic/.m2/repository/org/springframework/springloaded/1.2.0.RELEASE/springloaded-1.2.0.RELEASE.jar
 if [ ! -f $springloadedfile ]; then
 mvn validate -Psetup
 fi
-MAVEN_OPTS="-javaagent:$springloadedfile -noverify -Xms256m -Xmx2G -XX:PermSize=300m" mvn install -Prun
+MAVEN_OPTS="-javaagent:$springloadedfile -noverify -Xms512m -Xmx2G -XX:PermSize=512m" mvn install -Prun
